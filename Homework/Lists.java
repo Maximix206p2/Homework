4b704dfb6cb0a848b59ca5dea744d44c5836438e
Maximix206p2 <114424597+Maximix206p2@users.noreply.github.com>
2022-12-25 23:40:56 +0300

    Update Lists.java

--- Homework/Lists.java
+++ Homework/Lists.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class Lists {
     public static void main(String[] args) {
-        //ArrayList to LinkedList
+        //1.ArrayList to LinkedList
         //System.out.println(generateString(3,6));
         //Упаковка
         ArrayList<LinkedList<String>> bigBox = new ArrayList<>();
@@ -15,8 +15,8 @@ public class Lists {
             bigBox.add(smallBox);
         }
 
-        //Изменение элементов (2-ой маленькой коробки, 3-его элемента)
-        bigBox.get(2).set(3,"elem");
+        //Изменение элементов (3-ей маленькой коробки, 3-его элемента)
+        bigBox.get(3).set(3,"elem");
 
         //Замена маленькой коробки
         LinkedList<String> box = new LinkedList<>();
@@ -28,6 +28,34 @@ public class Lists {
         //Распаковка
         for (LinkedList<String> smallBox:bigBox) {
             for (String s : smallBox)
+                System.out.print(s + " ");
+            System.out.println();
+        }
+
+        System.out.println();
+
+        //2.ArrayList to Stack
+        ArrayList<Stack<String>> bigBox2 = new ArrayList<>();
+        for (int i=0; i<5; i++){
+            Stack<String> smallBox2 = new Stack<>();
+            for (int j=0; j<5; j++)
+                smallBox2.add(generateString(3,6));
+            bigBox2.add(smallBox2);
+        }
+
+        //Изменение элементов (4-ой маленькой коробки, 2-ого элемента)
+        bigBox2.get(4).set(2,"Forbidden");
+
+        //Замена маленькой коробки
+        Stack<String> box2 = new Stack<>();
+        for (int i = 0; i<4; i++)
+            box2.add("bang");
+
+        bigBox2.set(2,box2);
+
+        //Распаковка
+        for (Stack<String> smallBox2:bigBox2) {
+            for (String s : smallBox2)
                 System.out.print(s + " ");
             System.out.println();
         }
