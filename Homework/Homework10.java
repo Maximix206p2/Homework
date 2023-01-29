0832510f4ca8b685cbf9e5129b1e85cf9608223e
Maximix206p2 <114424597+Maximix206p2@users.noreply.github.com>
2023-01-29 23:22:56 +0300

    Add files via upload

--- Homework/Homework10.java
+++ Homework/Homework10.java
@@ -1,46 +1,41 @@
-        //Это вырвано из кода, если что, так что это надо вставить в общий код)
 
-        //Частота встречаемости букв в словах
-        char c0 = 'a';
-        int count0 = 0;
-        int countAll=0;
-        while (c0 != 'z' + 1) {
-            for (String s1 : vocab) {
-                char[] arr = s1.toCharArray();
-                Arrays.sort(arr);
-                for (char c : arr) {
-                    if (c == c0) count0++;
-                }
-            }
-            if (count0%10 == 2 || count0%10 == 3 || count0%10 == 4 && count0%100 != 12 && count0%100 != 13 && count0%100 != 14) //Это для красоты
-                System.out.printf("Буква %c встречается %d раза", c0, count0);
-            else
-                System.out.printf("Буква %c встречается %d раз", c0, count0);
-            System.out.println();
-            countAll += count0;
-            c0++;
-            count0 = 0;
-        }
-        System.out.printf("Всего символов - %d + «I»\n", countAll);
-        System.out.println();
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.FileSystemAlreadyExistsException;
+import java.util.Random;
 
-        //Количество слов, начинающихся на определенную букву (от 'a' до 'z')
-        c0 = 'a';
-        countAll = 0;
-        while (c0 != 'z' + 1) {
-            for (String s1 : vocab) {
-                char[] arr = s1.toCharArray();
-                    if (arr[0] == c0) count0++;
+public class Homework10 {
+    public static void main(String[] args) throws FileNotFoundException {
+        XmlReader[] xmlReaders = new XmlReader[5];
+        for (int i = 0; i < 5; i++)
+            xmlReaders[i] = new XmlReader();
+        runner(xmlReaders);
+    }
+
+    public static void runner(XmlReader[] xmlReaders) throws FileNotFoundException {
+        for (XmlReader xmlReader : xmlReaders) {
+            try {
+                xmlReader.read();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (FileSystemAlreadyExistsException e) {
+                throw new FileNotFoundException();
             }
-            if (count0%10 == 2 || count0%10 == 3 || count0%10 == 4 && count0%100 != 12 && count0%100 != 13 && count0%100 != 14) //Это для красоты
-                System.out.printf("%d слова на буква %c", count0, c0);
-            else if (count0%10 == 1 && count0%100 != 11)
-                System.out.printf("%d слово на буква %c", count0, c0);
-            else
-                System.out.printf("%d слов на буква %c", count0, c0);
-            System.out.println();
-            c0++;
-            countAll += count0;
-            count0 = 0;
         }
-        System.out.printf("Всего первых символов - %d + «I», что итак очевидно, но это для проверки)", countAll);
+    }
+}
+class XmlReader {
+    private static final Random random = new Random();
+    public void read() throws IOException{
+        switch (random.nextInt(3)) {
+            case 1:
+                throw new NullPointerException();
+            case 2:
+                throw new Error();
+            case 3:
+                throw new FileNotFoundException();
+            default:
+                throw new FileSystemAlreadyExistsException();
+        }
+    }
+}
