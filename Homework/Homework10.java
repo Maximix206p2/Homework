86bb566d79c7326ad6ab5f34d2a884281f055ddd
Maximix206p2 <114424597+Maximix206p2@users.noreply.github.com>
2023-01-29 23:52:22 +0300

    Update Homework10.java

--- Homework/Homework10.java
+++ Homework/Homework10.java
@@ -3,10 +3,33 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.FileSystemAlreadyExistsException;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Homework10 {
     public static void main(String[] args) throws FileNotFoundException {
-        //task6
+        //task 1
+        Random rand1 = new Random();
+        Random rand2 = new Random();
+        int numb = 0;
+        int count = rand2.nextInt(1, 10);
+        System.out.println(count);
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int number = scan.nextInt();
+        int finish;
+        for (int i = 0; i < count; i++) {
+            numb = rand1.nextInt(-10, 10);
+            try {
+                System.out.printf("%d / %d = ", number, numb);
+                finish = number / numb;
+                System.out.print(finish);
+                System.out.println();
+            } catch (Exception e) {
+                System.out.println("division by zero exception");
+            }
+        }
+
+        //task 6
         XmlReader[] xmlReaders = new XmlReader[5];
         for (int i = 0; i < 5; i++)
             xmlReaders[i] = new XmlReader();
@@ -25,9 +48,11 @@ public class Homework10 {
         }
     }
 }
+
 class XmlReader {
     private static final Random random = new Random();
-    public void read() throws IOException{
+
+    public void read() throws IOException {
         switch (random.nextInt(3)) {
             case 1:
                 throw new NullPointerException();
